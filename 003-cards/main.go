b2d48685d2a08e43807149ea25fba79cd0b57857
Wayne Yong <ymw0331@gmail.com>
2023-07-18 09:13:23 +0800

    add var declaration in go

---
+++ 003-cards/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// Functions and Returns
+
+func main() {
+	card := newCard()
+	fmt.Println(card)
+}
+
+// every function that returns a value must indicate what type of value it is returning
+func newCard() string {
+	return "Five of Diamonds"
+}
